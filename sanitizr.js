8e075f72de536ace8386ed1f86f73f7d18e625a7
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2013-10-17 09:55:15 -0500

    Tab stops
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

--- sanitizr.js
+++ sanitizr.js
@@ -1,36 +1,36 @@
 (function() {
+ 
+    // Replace all text numbers.
+    var walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        null,
+        false
+    );
 
-  // Replace all text numbers.
-  var walker = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    null,
-    false
-  );
-
-  function replaceFn() {
-    return ~~(Math.random() * 10);
-  }
-
-  while(node = walker.nextNode()) {
-    if (node.nodeValue != null && node.nodeValue.match(/\d/)) {
-      node.nodeValue = node.nodeValue.replace(/\d/g, replaceFn);
+    function replaceFn() {
+      return ~~(Math.random() * 10);
     }
-  }
-
-  // Replace all input values.
-  var nodeList = document.getElementsByTagName('input'), value;
-  for (var i = 0; i < nodeList.length; ++i) {
-    var node = nodeList[i];
-    if ((node.getAttribute('type') === 'number' || node.getAttribute('type') === 'text') && node.value != null && node.value.match(/\d/)) {
-      node.value = node.value.replace(/\d/g, replaceFn);
+ 
+    while(node = walker.nextNode()) {
+        if (node.nodeValue != null && node.nodeValue.match(/\d/)) {
+          node.nodeValue = node.nodeValue.replace(/\d/g, replaceFn);
+        }
     }
-  }
-
-  // Enable contenteditable for all elements.
-  var nodeList = document.getElementsByTagName('*');
-  for (var i = 0; i < nodeList.length; ++i) {
-    nodeList[i].setAttribute('contenteditable','true');
-  }
-
+    
+    // Replace all input values.
+    var nodeList = document.getElementsByTagName('input'), value;
+    for (var i = 0; i < nodeList.length; ++i) {
+        var node = nodeList[i];
+        if ((node.getAttribute('type') === 'number' || node.getAttribute('type') === 'text') && node.value != null && node.value.match(/\d/)) {
+          node.value = node.value.replace(/\d/g, replaceFn);
+        }
+    }
+    
+    // Enable contenteditable for all elements.
+    var nodeList = document.getElementsByTagName('*');
+    for (var i = 0; i < nodeList.length; ++i) {
+        nodeList[i].setAttribute('contenteditable','true');
+    }
+    
 })();
