13af127183385c4488ca28aa2097a7d1e27d73ad
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2013-10-17 09:53:28 -0500

    Code cleaning and adding support for input[type=number] fields
    Signed-off-by: Kevin Kinnebrew <kevin.kinnebrew@gmail.com>

--- sanitizr.js
+++ sanitizr.js
@@ -1,46 +1,36 @@
 (function() {
- 
-    // Replace all text numbers.
-    var walker = document.createTreeWalker(
-        document.body,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
-    );
- 
-    while(node = walker.nextNode()) {
-        if (node.nodeValue != null && node.nodeValue.match(/\d/g)) {
-            var len = node.nodeValue.length;
-            for (var i = 0; i < len; ++i)
-            {
-                var c = node.nodeValue.charAt(i);
-                if (c.match(/\d/g)) {
-                    node.nodeValue = node.nodeValue.replace(c, ~~(Math.random() * 10));
-                }
-            }
-        }
+
+  // Replace all text numbers.
+  var walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    null,
+    false
+  );
+
+  function replaceFn() {
+    return ~~(Math.random() * 10);
+  }
+
+  while(node = walker.nextNode()) {
+    if (node.nodeValue != null && node.nodeValue.match(/\d/)) {
+      node.nodeValue = node.nodeValue.replace(/\d/g, replaceFn);
     }
-    
-    // Replace all input values.
-    var nodeList = document.getElementsByTagName('input');
-    for (var i = 0; i < nodeList.length; ++i) {
-        var node = nodeList[i];
-        if (node.getAttribute('type') === 'text' && node.value != null && node.value.match(/\d/g)) {
-            var len = node.value.length;
-            for (var j = 0; j < len; ++j)
-            {
-                var c = node.value.charAt(j);
-                if (c.match(/\d/g)) {
-                    node.value = node.value.replace(c, ~~(Math.random() * 10));
-                }
-            }
-        }
+  }
+
+  // Replace all input values.
+  var nodeList = document.getElementsByTagName('input'), value;
+  for (var i = 0; i < nodeList.length; ++i) {
+    var node = nodeList[i];
+    if ((node.getAttribute('type') === 'number' || node.getAttribute('type') === 'text') && node.value != null && node.value.match(/\d/)) {
+      node.value = node.value.replace(/\d/g, replaceFn);
     }
-    
-    // Enable contenteditable for all elements.
-    var nodeList = document.getElementsByTagName('*');
-    for (var i = 0; i < nodeList.length; ++i) {
-        nodeList[i].setAttribute('contenteditable','true');
-    }
-    
+  }
+
+  // Enable contenteditable for all elements.
+  var nodeList = document.getElementsByTagName('*');
+  for (var i = 0; i < nodeList.length; ++i) {
+    nodeList[i].setAttribute('contenteditable','true');
+  }
+
 })();
